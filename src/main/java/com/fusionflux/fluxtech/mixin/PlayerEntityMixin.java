c1bbe0cd48c9407c02ae619c5fa34cf456b74c10
Platymemo <edwardhazelton+github@gmail.com>
2021-02-18 23:49:04 -0900

    Fix Lapis Tools

--- src/main/java/com/fusionflux/fluxtech/mixin/PlayerEntityMixin.java
+++ src/main/java/com/fusionflux/fluxtech/mixin/PlayerEntityMixin.java
@@ -4,34 +4,30 @@ import com.fusionflux.fluxtech.accessor.PlayerEntityExtensions;
 import com.fusionflux.fluxtech.config.FluxTechConfig2;
 import com.fusionflux.fluxtech.effects.CustomEffects;
 import com.fusionflux.fluxtech.items.FluxTechItems;
-import net.minecraft.block.BlockState;
-import net.minecraft.enchantment.EnchantmentHelper;
+import com.fusionflux.fluxtech.util.FluxTechTags;
 import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Mixin(PlayerEntity.class)
@@ -40,30 +36,27 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
     @Shadow
     @Final
     public PlayerAbilities abilities;
+    @Unique
     private int groundpound = 0;
+    @Unique
     private double fallSpeedMax = 0;
-
-
-
+    @Unique
     private EndCrystalEntity connectedCrystal;
-
     @Override
     public EndCrystalEntity fluxtech_getConnectedCrystal() {
         return connectedCrystal;
     }
 
-
-
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
+        throw new AssertionError("FluxTech: Called constructor for PlayerEntityMixin!");
     }
 
     @Shadow
     public abstract ItemStack getEquippedStack(EquipmentSlot slot);
 
-    @Shadow public abstract void playSound(SoundEvent sound, float volume, float pitch);
-
-    @Shadow @Final public PlayerInventory inventory;
+    @Shadow
+    public abstract void playSound(SoundEvent sound, float volume, float pitch);
 
     @Inject(method = "isInvulnerableTo", at = @At("HEAD"), cancellable = true)
     public void isInvulnerableTo(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
@@ -72,8 +65,6 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
             cir.setReturnValue(true);
         }
     }
-
-
 
     @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
     public void travel(Vec3d movementInput, CallbackInfo ci) {
@@ -105,7 +96,6 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void tick(CallbackInfo ci) {
             if (!this.isDead()) {
-                BlockPos testpos = new BlockPos(this.getPos().x, this.getPos().y - 1.5, this.getPos().z);
                 if (this.connectedCrystal != null) {
                     this.applyStatusEffect(new StatusEffectInstance(CustomEffects.DRAGONHEALING, 1205, 0));
                     if (this.connectedCrystal.removed) {
@@ -121,21 +111,16 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
                 List<EndCrystalEntity> list = this.world.getNonSpectatingEntities(EndCrystalEntity.class, this.getBoundingBox().expand(32.0D));
                 EndCrystalEntity endCrystalEntity = null;
                 double d = 1000;
-                Iterator var5 = list.iterator();
-
-                while (var5.hasNext()) {
-                    EndCrystalEntity endCrystalEntity2 = (EndCrystalEntity) var5.next();
+                for (EndCrystalEntity endCrystalEntity2 : list) {
                     double e = endCrystalEntity2.squaredDistanceTo(this);
 
                     if (e < d) {
                         d = e;
                         endCrystalEntity = endCrystalEntity2;
-                        // endCrystalEntity.setBeamTarget(testpos);
                     }
                 }
 
                 this.connectedCrystal = endCrystalEntity;
-                //}
             }
             ItemStack itemStack5 = this.getEquippedStack(EquipmentSlot.FEET);
             if (!this.isOnGround() && (itemStack5.getItem().equals(FluxTechItems.GRAVITRONS) || (itemStack5.getItem().equals(FluxTechItems.SLIME_COATED_NETHERITE_BOOTS) /*|| itemStack5.getItem().equals(FluxTechItems.UNSTABLE_GRAVITRONS)*/))) {
@@ -151,7 +136,7 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
             if (!this.isTouchingWater() && !this.isSneaking() && fallSpeedMax > .3 && this.isOnGround() && itemStack5.getItem().equals(FluxTechItems.SLIME_COATED_NETHERITE_BOOTS)) {
                 if (vec3d.y < 0.0D) {
                     //double d = this instanceof LivingEntity ? 1.0D : 1.8D;
-                    this.setVelocity(vec3d.x, fallSpeedMax * .8, vec3d.z);
+                    this.setVelocity(vec3d.x, fallSpeedMax * 0.8D, vec3d.z);
                     this.playSound(SoundEvents.BLOCK_SLIME_BLOCK_PLACE, 1, 1);
                     if (this.isOnGround() || this.isFallFlying() || this.isSneaking()) {
                         groundpound = 0;
@@ -187,12 +172,12 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
     @ModifyVariable(method = "getBlockBreakingSpeed", at = @At(value = "JUMP", opcode = Opcodes.IFLE, ordinal = 0))
     public float getBlockBreakingSpeed(float f) {
         ItemStack itemStack3 = this.getEquippedStack(EquipmentSlot.MAINHAND);
-        if (this.isSubmergedIn(FluidTags.WATER) && (itemStack3.getItem().equals(FluxTechItems.LAPIS_AXE)||itemStack3.getItem().equals(FluxTechItems.LAPIS_PICKAXE)||itemStack3.getItem().equals(FluxTechItems.LAPIS_HOE)||itemStack3.getItem().equals(FluxTechItems.LAPIS_SHOVEL))) {
+        if (this.isSubmergedIn(FluidTags.WATER) && (itemStack3.getItem().isIn(FluxTechTags.LAPIS_TOOLS))) {
             f *= 15.0F;
         }
         return f;
     }
-    }
+}
 
 
 
