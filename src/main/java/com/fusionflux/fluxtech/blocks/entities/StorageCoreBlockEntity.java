52f6fd9beabc311799e491e6f37920e13eac94d3
Fusion Flux <c4gamer2.0@gmail.com>
2021-04-27 23:01:08 -0500

    start of storage system code

--- src/main/java/com/fusionflux/fluxtech/blocks/entities/StorageCoreBlockEntity.java
+++ src/main/java/com/fusionflux/fluxtech/blocks/entities/StorageCoreBlockEntity.java
@@ -11,22 +11,53 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Nameable;
+import net.minecraft.util.math.BlockPos;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class StorageCoreBlockEntity extends BlockEntity implements Nameable {
     private ItemStack item = ItemStack.EMPTY;
-    private LinkedList<StorageNodeBlockEntity> connectedNodes = new LinkedList<StorageNodeBlockEntity>();
+    //private LinkedList<StorageNodeBlockEntity> connectedNodes = new LinkedList<StorageNodeBlockEntity>();
+    public final List<BlockPos> connectedNodes = new ArrayList<>();
 
     public StorageCoreBlockEntity() {
         super(FluxTechBlocks.STORAGE_CORE_BLOCK_ENTITY);
     }
 
-    public void addNewNodes(StorageNodeBlockEntity node){
-        connectedNodes.add(node);
-        System.out.println(connectedNodes.toString());
+    public void addNewNodes(BlockPos nodeBlockPos){
+        connectedNodes.add(nodeBlockPos);
+        System.out.println(connectedNodes);
     }
 
+    @Override
+    public void fromTag(BlockState state, CompoundTag tag) {
+        super.fromTag(state, tag);
+        int size = tag.getInt("size");
+        for (int i = 0; i < size; i++) {
+            connectedNodes.add(new BlockPos(
+                    tag.getInt(i + "x"),
+                    tag.getInt(i + "y"),
+                    tag.getInt(i + "z")
+            ));
+        }
+
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+
+        tag.putInt("size", connectedNodes.size());
+        for (int i = 0; i < connectedNodes.size(); i++) {
+            tag.putInt(i + "x", connectedNodes.get(i).getX());
+            tag.putInt(i + "y", connectedNodes.get(i).getY());
+            tag.putInt(i + "z", connectedNodes.get(i).getZ());
+        }
+
+        return tag;
+    }
 
     @Override
     public Text getName() {
--- src/main/java/com/fusionflux/fluxtech/blocks/entities/StorageNodeBlockEntity.java
+++ src/main/java/com/fusionflux/fluxtech/blocks/entities/StorageNodeBlockEntity.java
@@ -15,12 +15,14 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.ScheduledTick;
 import net.minecraft.world.World;
+import org.lwjgl.system.CallbackI;
 
 import java.util.Objects;
 
 public class StorageNodeBlockEntity extends BlockEntity implements Inventory, Nameable {
     private ItemStack item = ItemStack.EMPTY;
     private StorageCoreBlockEntity savedCore;
+    private BlockPos connectedCore;
     public StorageNodeBlockEntity() {
         super(FluxTechBlocks.STORAGE_NODE_BLOCK_ENTITY);
     }
@@ -38,51 +40,52 @@ public class StorageNodeBlockEntity extends BlockEntity implements Inventory, Na
         StorageNodeBlockEntity node;
         if (this.world != null) {
             if (!this.world.isClient) {
-                for (int i = 1; i < 7; i++) {
-                    Direction offsetdir = Direction.NORTH;
-                    switch (i) {
-                        case 1:
-                            offsetdir = Direction.NORTH;
-                            break;
-                        case 2:
-                            offsetdir = Direction.EAST;
-                            break;
-                        case 3:
-                            offsetdir = Direction.SOUTH;
-                            break;
-                        case 4:
-                            offsetdir = Direction.WEST;
-                            break;
-                        case 5:
-                            offsetdir = Direction.UP;
-                            break;
-                        case 6:
-                            offsetdir = Direction.DOWN;
-                            break;
-                    }
+                if(connectedCore==null) {
+                        for (int i = 1; i < 7; i++) {
+                            Direction offsetdir = Direction.NORTH;
+                            switch (i) {
+                                case 1:
+                                    offsetdir = Direction.NORTH;
+                                    break;
+                                case 2:
+                                    offsetdir = Direction.EAST;
+                                    break;
+                                case 3:
+                                    offsetdir = Direction.SOUTH;
+                                    break;
+                                case 4:
+                                    offsetdir = Direction.WEST;
+                                    break;
+                                case 5:
+                                    offsetdir = Direction.UP;
+                                    break;
+                                case 6:
+                                    offsetdir = Direction.DOWN;
+                                    break;
+                            }
 
-                    if (this.world.getBlockState(this.getPos().offset(offsetdir)).getBlock().equals(FluxTechBlocks.STORAGE_NODE_BLOCK)) {
-                        node = (StorageNodeBlockEntity) this.world.getBlockEntity(new BlockPos(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ()).offset(offsetdir));
-                        if (node.savedCore != null) {
-                            core = node.savedCore;
-                            assert core != null;
-                            savedCore = core;
-                            core.addNewNodes(this);
-                            System.out.println("node");
+                            if (this.world.getBlockState(this.getPos().offset(offsetdir)).getBlock().equals(FluxTechBlocks.STORAGE_NODE_BLOCK)) {
+                                node = (StorageNodeBlockEntity) this.world.getBlockEntity(new BlockPos(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ()).offset(offsetdir));
+                                if (node.connectedCore != null) {
+                                    this.connectedCore = node.connectedCore;
+                                    core = (StorageCoreBlockEntity) this.world.getBlockEntity(node.connectedCore);
+                                    core.addNewNodes(this.pos);
+                                    System.out.println("node");
+                                }
+                                break;
+                            }
+                            if (this.world.getBlockState(this.getPos().offset(offsetdir)).getBlock().equals(FluxTechBlocks.STORAGE_CORE_BLOCK)) {
+                                core = (StorageCoreBlockEntity) this.world.getBlockEntity(new BlockPos(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ()).offset(offsetdir));
+                                connectedCore = core.getPos();
+                                core.addNewNodes(this.pos);
+                                System.out.println("core");
+                                break;
+                            }
+                            if (i >= 6) {
+                                connectedCore=new BlockPos(0,-5,0);
+                                break;
+                            }
                         }
-                        break;
-                    }
-                    if (this.world.getBlockState(this.getPos().offset(offsetdir)).getBlock().equals(FluxTechBlocks.STORAGE_CORE_BLOCK)) {
-                        core = (StorageCoreBlockEntity) this.world.getBlockEntity(new BlockPos(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ()).offset(offsetdir));
-                        savedCore = core;
-                        assert core != null;
-                        core.addNewNodes(this);
-                        System.out.println("core");
-                        break;
-                    }
-                    if (i >= 6) {
-                        break;
-                    }
                 }
             }
         }
@@ -133,6 +136,12 @@ public class StorageNodeBlockEntity extends BlockEntity implements Inventory, Na
         super.fromTag(state, tag);
         this.item = ItemStack.fromTag(tag.getCompound("item"));
         this.item.setCount(tag.getInt("itemCount"));
+        connectedCore = (new BlockPos(
+                tag.getInt("x"),
+                tag.getInt("y"),
+                tag.getInt("z")
+        ));
+
     }
 
     @Override
@@ -142,6 +151,11 @@ public class StorageNodeBlockEntity extends BlockEntity implements Inventory, Na
         this.item.toTag(itemTag);
         tag.put("item", itemTag);
         tag.putInt("itemCount", item.getCount());
+
+        tag.putInt("x", connectedCore.getX());
+        tag.putInt("y", connectedCore.getY());
+        tag.putInt("z", connectedCore.getZ());
+
         return tag;
     }
 
