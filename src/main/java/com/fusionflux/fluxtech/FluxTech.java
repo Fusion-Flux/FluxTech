1f624f4aee49215924f9e7da10888dee2d2e2d24
Platymemo <edwardhazelton+github@gmail.com>
2021-05-08 01:56:55 -0800

    remove old screenhandler stuff

--- src/main/java/com/fusionflux/fluxtech/FluxTech.java
+++ src/main/java/com/fusionflux/fluxtech/FluxTech.java
@@ -6,7 +6,7 @@ import com.fusionflux.fluxtech.delay.DelayedForLoopManager;
 import com.fusionflux.fluxtech.effects.CustomEffects;
 import com.fusionflux.fluxtech.items.FluxTechItems;
 import com.fusionflux.fluxtech.sound.FluxTechSounds;
-//import com.oroarmor.util.config.ConfigItemGroup;
+import com.fusionflux.fluxtech.teb.CloakingEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -18,19 +18,12 @@ public class FluxTech implements ModInitializer {
 
     public static final String MOD_ID = "fluxtech";
 
-    //public static final ScreenHandlerType<BoxScreenHandler> BOX_SCREEN_HANDLER;
-
     public static final ItemGroup FLUXTECH_GROUP = FabricItemGroupBuilder.build(
             new Identifier("fluxtech", "general"),
             () -> new ItemStack(FluxTechItems.HANDHELD_PROPULSION_DEVICE));
 
-    static{
-       // BOX_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier(MOD_ID, "core"), BoxScreenHandler::new);
-    }
-
     @Override
     public void onInitialize() {
-        //ScreenRegistry.register(FluxTech.BOX_SCREEN_HANDLER, BoxScreen::new);
         //processConfig();
         FluxTechConfig2.register();
         CustomEffects.registerEffects();
@@ -40,11 +33,14 @@ public class FluxTech implements ModInitializer {
         ServerTickEvents.END_SERVER_TICK.register(minecraftServer -> {
             DelayedForLoopManager.tick();
         });
+        CloakingEvents.registerCloakingEvents();
 
     }
-   /* private void processConfig() {
+    /*
+    private void processConfig() {
         CONFIG.readConfigFromFile();
 
         ServerLifecycleEvents.SERVER_STOPPED.register(l -> CONFIG.saveConfigToFile());
-    }*/
+    }
+    */
 }
