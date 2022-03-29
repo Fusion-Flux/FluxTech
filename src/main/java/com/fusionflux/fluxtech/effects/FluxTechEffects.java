0bc26712ea694c986dce621552fe4d06ea7222ff
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2022-03-29 09:26:03 -0500

    saving whatever this is before i rewrite everything

--- src/main/java/com/fusionflux/fluxtech/effects/FluxTechEffects.java
+++ src/main/java/com/fusionflux/fluxtech/effects/FluxTechEffects.java
@@ -2,12 +2,15 @@ package com.fusionflux.fluxtech.effects;
 
 import com.fusionflux.fluxtech.FluxTech;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+
+
 public class FluxTechEffects {
-    public static final StatusEffect DRAGONHEALING = new DragonHealing(StatusEffectType.BENEFICIAL,0x98D982);
+    public static final StatusEffect DRAGONHEALING = new DragonHealing(StatusEffectCategory.BENEFICIAL,0x98D982);
 
     public static void registerEffects() {
         Registry.register(Registry.STATUS_EFFECT,  new Identifier(FluxTech.MOD_ID, "dragon_healing"), DRAGONHEALING);
--- src/main/java/com/fusionflux/fluxtech/mixin/PlayerEntityMixin.java
+++ src/main/java/com/fusionflux/fluxtech/mixin/PlayerEntityMixin.java
@@ -5,6 +5,7 @@ import com.fusionflux.fluxtech.items.FluxTechItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,12 +13,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -34,12 +33,22 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     @Shadow
     @Final
     public PlayerAbilities abilities;
+
+    @Shadow public abstract void increaseTravelMotionStats(double dx, double dy, double dz);
+
+
+    @Shadow protected abstract boolean clipAtLedge();
+
+    @Shadow protected abstract boolean method_30263();
+
     @Unique
     private boolean fluxtech$doGroundPound;
     @Unique
     private double fluxtech$fallSpeedMax;
     @Unique
     private boolean fluxtech$doCrunch;
+
+    public boolean fallPls = false;
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
@@ -55,7 +64,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             cir.setReturnValue(true);
         }
     }
-
+/*
     @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
     public void travel(Vec3d movementInput, CallbackInfo ci) {
         ItemStack legStack = this.getEquippedStack(EquipmentSlot.LEGS);
@@ -80,13 +89,147 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         }
 
     }
+*/
+
+@Override
+    public Vec3d adjustMovementForSneaking(Vec3d movement, MovementType type) {
+        if (!this.abilities.flying && (type == MovementType.SELF || type == MovementType.PLAYER) && this.clipAtLedge() && this.method_30263()&& fallPls) {
+            double d = movement.x;
+            double e = movement.z;
+            double var7 = 0.05D;
+
+            while(true) {
+                while(d != 0.0D && this.world.isSpaceEmpty(this, this.getBoundingBox().offset(d, (double)(-this.stepHeight), 0.0D))) {
+                    if (d < 0.05D && d >= -0.05D) {
+                        d = 0.0D;
+                    } else if (d > 0.0D) {
+                        d -= 0.05D;
+                    } else {
+                        d += 0.05D;
+                    }
+                }
+
+                while(true) {
+                    while(e != 0.0D && this.world.isSpaceEmpty(this, this.getBoundingBox().offset(0.0D, (double)(-this.stepHeight), e))) {
+                        if (e < 0.05D && e >= -0.05D) {
+                            e = 0.0D;
+                        } else if (e > 0.0D) {
+                            e -= 0.05D;
+                        } else {
+                            e += 0.05D;
+                        }
+                    }
+
+                    while(true) {
+                        while(d != 0.0D && e != 0.0D && this.world.isSpaceEmpty(this, this.getBoundingBox().offset(d, (double)(-this.stepHeight), e))) {
+                            if (d < 0.05D && d >= -0.05D) {
+                                d = 0.0D;
+                            } else if (d > 0.0D) {
+                                d -= 0.05D;
+                            } else {
+                                d += 0.05D;
+                            }
+
+                            if (e < 0.05D && e >= -0.05D) {
+                                e = 0.0D;
+                            } else if (e > 0.0D) {
+                                e -= 0.05D;
+                            } else {
+                                e += 0.05D;
+                            }
+                        }
+
+                        movement = new Vec3d(d, movement.y, e);
+                        return movement;
+                    }
+                }
+            }
+        } else {
+            return movement;
+        }
+    }
+
+
+    /**
+     * @author
+     */
+    @Overwrite
+    public void travel(Vec3d movementInput) {
+        if (!this.isOnGround() && !this.abilities.flying && !this.isFallFlying()) {
+
+            double mathval = 1;
+            double horizontalvelocity = Math.abs(this.getVelocity().x)+Math.abs(this.getVelocity().z);
+            if(horizontalvelocity/0.01783440120041885 > 1){
+                mathval= horizontalvelocity/0.01783440120041885;
+            }
+
+            movementInput = new Vec3d(movementInput.x/mathval,movementInput.y,movementInput.z/mathval);
+            //movementInput=movementInput.multiply(1, 1, 0);
+           /* float airSpeed = 0;
+            float scalevalue;
+
+            if(horizontalvelocity<1){
+               scalevalue = 1.5f;
+            }else{
+                scalevalue =(float)horizontalvelocity*4;
+            }
+
+            if(this.isSneaking() && horizontalvelocity > .5){
+                airSpeed =(.01f*(10f/3f))*scalevalue;
+
+            }else{
+                airSpeed =.01f*scalevalue;
+            }
+            this.airStrafingSpeed = airSpeed;*/
+        }
+       /* double horizontalvelocity = Math.abs(this.getVelocity().x)+Math.abs(this.getVelocity().z);
+        if(this.isSneaking() && horizontalvelocity > 1){
+            fallPls=false;
+            this.stepHeight = 1.6f;
+            this.horizontalCollision = false;
+        }else{
+            fallPls=true;
+            this.stepHeight = .6f;
+        }*/
+
+
+
+        double d = this.getX();
+        double e = this.getY();
+        double f = this.getZ();
+        double g;
+        if (this.isSwimming() && !this.hasVehicle()) {
+            g = this.getRotationVector().y;
+            double h = g < -0.2D ? 0.085D : 0.06D;
+            if (g <= 0.0D || this.jumping || !this.world.getBlockState(new BlockPos(this.getX(), this.getY() + 1.0D - 0.1D, this.getZ())).getFluidState().isEmpty()) {
+                Vec3d vec3d = this.getVelocity();
+                this.setVelocity(vec3d.add(0.0D, (g - vec3d.y) * h, 0.0D));
+            }
+        }
+
+        if (this.abilities.flying && !this.hasVehicle()) {
+            g = this.getVelocity().y;
+            float h = this.airStrafingSpeed;
+            this.airStrafingSpeed = this.abilities.getFlySpeed() * (float)(this.isSprinting() ? 2 : 1);
+            super.travel(movementInput);
+            Vec3d vec3d2 = this.getVelocity();
+            this.setVelocity(vec3d2.x, g * 0.6D, vec3d2.z);
+            this.airStrafingSpeed = h;
+            this.onLanding();
+            this.setFlag(7, false);
+        } else {
+            super.travel(movementInput);
+        }
+
+        this.increaseTravelMotionStats(this.getX() - d, this.getY() - e, this.getZ() - f);
+    }
 
     private float getSlipSpeed(float slipperiness) {
         return this.getMovementSpeed() * (0.21600002F / (slipperiness * slipperiness * slipperiness));
     }
 
-    @Inject(method="getFallSound", at = @At("HEAD"), cancellable = true)
-    protected void getFallSound(int distance, CallbackInfoReturnable<SoundEvent> cir){
+    /*@Inject(method="getFallSounds", at = @At("HEAD"), cancellable = true)
+    protected void getFallSounds(CallbackInfoReturnable<FallSounds> cir){
         ItemStack feetStack = this.getEquippedStack(EquipmentSlot.FEET);
         if(feetStack.getItem() == FluxTechItems.GRAVITRONS){
             cir.setReturnValue(SoundEvents.BLOCK_NETHERITE_BLOCK_FALL);
@@ -95,10 +238,17 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         if(feetStack.getItem() == FluxTechItems.SLIME_COATED_NETHERITE_BOOTS){
             cir.setReturnValue(SoundEvents.BLOCK_SLIME_BLOCK_PLACE);
         }
-    }
+    }*/
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void tick(CallbackInfo ci) {
+
+        double horizontalvelocity2 = Math.abs(this.getVelocity().x)+Math.abs(this.getVelocity().z);
+        //this.setNoDrag(!this.isOnGround() && !this.abilities.flying && !this.isFallFlying());
+        this.setNoDrag((!this.isOnGround() && !this.abilities.flying && !this.isFallFlying()) || (this.isSneaking() && horizontalvelocity2 > 1.5));
+
+
+
         ItemStack feetStack = this.getEquippedStack(EquipmentSlot.FEET);
         if (!this.isOnGround() && (feetStack.getItem() == FluxTechItems.SLIDERS) || feetStack.getItem() == FluxTechItems.SLIME_COATED_NETHERITE_BOOTS) {
             if (this.getVelocity().y < fluxtech$fallSpeedMax && this.getVelocity().y < 0) {
@@ -108,7 +258,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             if (this.getVelocity().y < fluxtech$fallSpeedMax && this.getVelocity().y < 0) {
                 fluxtech$fallSpeedMax = Math.abs(this.getVelocity().y);
             }
-            
+
             if (this.getVelocity().y < -1) {
                 fluxtech$doGroundPound = true;
             }
